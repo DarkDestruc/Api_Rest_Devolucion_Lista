abd9befe8b8f949ee234e65ebc21d246e6c8e877
Moises Chavez <gchavez19956@gmail.com>
2024-03-08 01:37:34 -0500

    Add project files.

---
+++ Api_Rest_Devolucion_Lista/Controllers/List_Controller.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api_Rest_Devolucion_Lista.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class List_Controller : ControllerBase
+    {
+        private static List<string> helados = new List<string> { "Corneto", "Magnun", "Polito" };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> GeList_Controller()
+        {
+            return Ok(helados);
+
+        }
+
+    }
+
+
+
+
+
+
+}
